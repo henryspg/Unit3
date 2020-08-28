d338deffc71c30346e61883d690a354d146d37e5
Henry Gultom <39720108+henryspg@users.noreply.github.com>
2020-08-28 16:03:40 -0700

    updated 08-28

--- app02.py
+++ app02.py
@@ -1,4 +1,3 @@
-from flask import Flask, request, jsonify
 import random
 import os
 import pickle
@@ -8,47 +7,49 @@ from abnbsfbw import predict1
 
 
 app = FastAPI(
-    title='DS API Airbnb aug-2020',
-    description='Under review by Henry Gultom',
+    title='Data Science API project: Airbnb SF',
+    description='By: Henry Gultom, \nLambda School student',
     version='0.1',
     docs_url='/',
 )
 
+
 @app.get('/')
 def root():
-    """hello from Henry, DS17"""
+    """To get the price prediction, please scroll down and click the green box: **POST /predict** """
     return {"Hello" : "everyone"}
 
 
 # Just in case we need user info
-@app.get('/user/{username}')
-def user(username):
-    # return "Hello user"
-    object_to_return = {
-        "username":username,
-        "num_of_requests":random.randint(0, 9),
-    }
-    return object_to_return # jsonify(object_to_return)
+# @app.get('/user/{username}')
+# def user(username):
+#     # return "Hello user"
+#     object_to_return = {
+#         "username":username,
+#         "num_of_requests":random.randint(0, 9),
+#     }
+#     return object_to_return
 
 
 @app.post('/predict')
-def predict(accommodates:int = 2, bedrooms: int = 1, bathrooms:float = 1.5,  beds: int=1, guests_included: int=1, minimum_nights: int=1, maximum_nights: int=2):
-    """ On this page we will predict the price based on below features"""
+def predict(accommodates:int = 1, bedrooms: int = 1, bathrooms:float = 1.0,  beds: int=1, guests_included: int=1, minimum_nights: int=1, maximum_nights: int=2):
+    """ 
+    1. click: **Try it out**  and change the value under the **Description**, and click: **Execute**.\n
+    2. It predicts Airbnb rent price using XGBOOST as Machine Learning model.\n
+    3. See the result below at: **Responses**  >>  **Details**  >>  **Response body**
+    """ 
 
     pred = predict1(accommodates, bedrooms, bathrooms, beds,	minimum_nights,	maximum_nights,	guests_included)
-    # y_pred = random.randint(20, 300)##temporary number
     res = {
-        "status": "success",
-        "Accommodates" : accommodates,
-        "bedrooms": bedrooms,
-        "bathrooms": bathrooms,    
-        "Beds": beds,
-        "Guest_included": guests_included,
-        "Minimum_nights": minimum_nights,
-        "Maximum_nights": maximum_nights,
-        "predicted_price": float(pred)
-        # "predicted_price": predict(Accommodates, Bathrooms, Bedrooms, Beds, Minimum_Nights , Maximum_Nights, Guests_Included)
-    }
+        "Accommodates"      : accommodates,
+        "bedrooms"          : bedrooms,
+        "bathrooms"         : bathrooms,    
+        "Beds"              : beds,
+        "Guest_included"    : guests_included,
+        "Minimum_nights"    : minimum_nights,
+        "Maximum_nights"    : maximum_nights,
+        "predicted_price"   : round(float(pred),1)
+        }
 
     return res
 
