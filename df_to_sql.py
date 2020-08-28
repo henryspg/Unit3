4e472314066b4200058ee22eafd1d1d0e1d510af
Henry Gultom <39720108+henryspg@users.noreply.github.com>
2020-08-28 08:42:51 -0700

    Add files via upload
    updated DB

--- df_to_sql.py
+++ df_to_sql.py
@@ -1,11 +1,11 @@
 import numpy as np
 import pandas as pd
-from pandas import DataFrame
+# from pandas import DataFrame
 import sqlite3
-from sqlalchemy import create_engine
+# from sqlalchemy import create_engine
 
 
-# 1- Connect our python with SQLITE Dbase. If the DBASE doesnt exist, it will create one
+# 1- Connect python with SQLITE Dbase. If the DBASE doesnt exist, it will create one
 conn = sqlite3.connect('airbnb.sqlite3')
 curs = conn.cursor()
 
@@ -33,9 +33,9 @@ conn.commit()
 # 3b- Now the table is PREPARED in the DAtaBase
 
 # 4- get the dataframe and update the column names
-#     If you dont update the column name, it will follow whatever csv file gives you 
-df = pd.read_csv('airbnb-listings.csv', usecols=[36, 51, 50, 52, 49,61, 63,64,56])
-df.columns = ['Neighborhood', 'Bedrooms', 'Bathrooms', 'Beds', 'Accommodates', 'Guests_Included', 'Minimum_Nights', 'Maximum_Nights', 'Price' ]
+#     If we dont update the column name, it will follow whatever csv file gives us 
+df = pd.read_csv('airbnb-listings.csv', usecols=[36, 44, 51, 50, 52, 49,61, 63,64,56])
+df.columns = ['Neighborhood', 'Country', 'Bedrooms', 'Bathrooms', 'Beds', 'Accommodates', 'Guests_Included', 'Minimum_Nights', 'Maximum_Nights', 'Price' ]
 
 
 # 5- Insert Pandas dataframe into SQLITE DataBase 
@@ -45,22 +45,21 @@ df.to_sql('airbnb', con = conn, if_exists='replace', index=False)
 conn.commit() 
 
 
-# 6- Now we can test on this python or using SQLITE apps.
+# 6- Now we can TEST on this python or using SQLITE apps.
 a = conn.execute("""SELECT * FROM airbnb
 	LIMIT 3""").fetchall()
 print("\nshow some records only: \n" )
-# [print(row) for row in a]
 for row in a:
   print(row)
 
 
 b = conn.execute("""
 select count(*), Neighborhood from airbnb
-where Bedrooms = 2
+where Bedrooms >1 AND Country = 'United States' 
 GROUP BY Neighborhood; 
 """  ).fetchall()
 
-print("\n count of 2-bedrooms per neighborhood: \n")
+print("\n count of >1 bedrooms per neighborhood in US: \n")
 [print(row) for row in b]
 
 
